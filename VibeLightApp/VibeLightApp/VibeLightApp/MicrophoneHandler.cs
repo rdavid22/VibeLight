b5d53ad3a6fe26ac64ce0d90a31bb79fc5c94e35
poseidon99 <haszonits.gergo@gmail.com>
2021-11-08 20:44:02 +0100

    Native & microphone

---
+++ VibeLightApp/VibeLightApp/VibeLightApp/MicrophoneHandler.cs
@@ -0,0 +1,187 @@
+﻿using Dyme.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VibelightApp
+{
+    public class MicrophoneHandler : Microphone
+    {
+        SimpleServiceManager simpleService = new SimpleServiceManager("glob");
+        SimpleServiceOptions options = new SimpleServiceOptions();
+        private bool Reading = false;
+        private double TargetSensitivity = 0; // dinamikusan álítodik
+        private int RefreshRate = 5; // ms
+        private int TargetAmplitude = 3500;
+        private int SmoothingValue = 10;
+
+        public MicrophoneHandler()
+        {
+           
+            Construct();
+            options.ActionNamesAndTitles = new Dictionary<string, string>()
+            {
+            { "Action1", "Stop" }
+
+            };
+
+        } 
+
+        private async void Construct()
+        {
+            await Initialize();
+        }
+
+
+        public async void StartReading(string message)
+        {
+            if (!IsInitialized() || Reading) { return; }
+
+            simpleService = await SimpleServiceManager.Start<MyService>("Effect Running", "", options);
+
+            long SmoothedValue = 0;
+            double SensitivityValue = 20;
+            string FormattedSend = "";
+            long cycle = 0;
+            int[] ValueArray = new int[SmoothingValue];
+
+            Reading = true;
+
+            await Start();
+
+            await Task.Run(async () =>
+            {
+                while (Reading)
+                {
+                    await Task.Delay(RefreshRate);
+                    FormattedSend = "";
+                    string send = message;
+                    SmoothedValue = 0;
+                    if (cycle == SmoothingValue)
+                    {
+                        cycle = 0;
+                    }
+                    for (int i = 0; i < SmoothingValue; i++)
+                    {
+                        SmoothedValue = SmoothedValue + ValueArray[i];
+
+                    }
+                    SmoothedValue = SmoothedValue / SmoothingValue;
+                    ValueArray[cycle] = Convert.ToInt32(Convert.ToDouble(Read()) * SensitivityValue);
+                    FormattedSend = "";
+
+                    MicrophoneSensitivity(ref SensitivityValue, ref SmoothedValue);
+                    while (send.Contains("["))
+                    {
+
+                        long minimum = Convert.ToInt64(send.Substring(send.IndexOf("[") + 1, send.IndexOf("_") - send.IndexOf("[") - 1));
+                        long maximum = Convert.ToInt64(send.Substring(send.IndexOf("_") + 1, send.IndexOf("]") - send.IndexOf("_") - 1));
+                        long difference = Math.Abs(maximum - minimum);
+                        long finalValue = 0;
+                        if (maximum > minimum)
+                        {
+                            finalValue = Convert.ToInt64(minimum + (Convert.ToDouble(SmoothedValue) / 10000 * Convert.ToDouble(difference)));
+
+                        }
+                        else
+                        {
+                            finalValue = Convert.ToInt64(minimum - (Convert.ToDouble(SmoothedValue) / 10000 * Convert.ToDouble(difference)));
+
+                        }
+                        FormattedSend += send.Substring(0, send.IndexOf("[")) + Convert.ToString(finalValue);
+
+                        send = send.Remove(0, send.IndexOf("]") + 1);
+                        if (!send.Contains("["))
+                        {
+                            FormattedSend += send;
+                        }
+
+                    }
+                    // Console.WriteLine(FormattedSend)
+                    cycle++;
+                    App.Message.MessageSendAsync(App.SelectedDevice, FormattedSend);
+
+                }
+            });
+            simpleService.StopService(removeNotification: true);
+            await Stop();
+
+        }
+
+
+
+        private void MicrophoneSensitivity(ref double sensitivity, ref long amplitude)
+        {
+
+
+            if (amplitude != 0)
+            {
+                long OverFlowValue = amplitude;
+
+                TargetSensitivity = (TargetAmplitude / Convert.ToDouble(amplitude)) * sensitivity;
+
+                if (amplitude > 10000)
+                {
+                    amplitude = 10000;
+                }
+                double AddingValue = Math.Abs(sensitivity - TargetSensitivity);
+                AddingValue = AddingValue * 0.001;
+
+                if (sensitivity > TargetSensitivity && sensitivity > 1)
+                {
+                    sensitivity = sensitivity - AddingValue;
+                }
+                else if (sensitivity < 100)
+                {
+                    sensitivity = sensitivity + AddingValue;
+
+                }
+            }
+        }
+
+        public void StopReading()
+        {
+            Reading = false;
+           
+        }
+    }
+    public class MyService : ISimpleService
+    {
+        public void OnExecuteAction(string actionName, IList<string> args)
+        {
+            // Event handler for actions declared in the "SimpleServiceOptions"
+            if (actionName == "Action1") { Exit(); }
+
+        }
+
+        public void OnStart(SimpleServiceCore core)
+        {
+        }
+
+
+
+        public void OnStop(IList<string> args)
+        {
+
+        }
+
+        public void OnTapNotification()
+        {
+        }
+        #region SERVICE METHODS
+
+        private void Exit()
+        {
+            App.Message.MessageSendAsync(App.SelectedDevice,"end");
+            App.Mic.StopReading();
+            MessagingCenter.Send<object, string>(this, SimpleServiceCore.CALL_STOP_SERVICE, null);
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        #endregion
+    }
+}
+
+
---
+++ VibeLightApp/VibeLightApp/VibeLightApp/NativeCodes.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VibelightApp
+{
+
+    public interface IAndroidBluetooth
+    {
+        List<string> PairedDevices();
+        Task<bool> Connect(string device);
+        Task Send(string message);
+        Task<string> Read();
+    }
+
+    public interface IAndroidWifi
+    {
+        string GetIp();
+    }
+
+    public interface IAndroidMicrophone
+    {
+        Task<bool> InitializeMicrophoneAsync();
+        Task StartMicrophoneAsync();
+        Task StopMicrophoneAsync();
+        int GetAmplitude();
+        bool IsInitialized();
+        bool IsStarted();
+    }
+
+    class Bluetooth
+    {
+        public List<string> PairedDevices()
+        {
+            List<string> controllers = new List<string>();
+
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                controllers = DependencyService.Get<IAndroidBluetooth>().PairedDevices();
+                return controllers;
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                // IOS CODE
+            }
+            return controllers;
+        }
+        public async Task<bool> Connect(string device)
+        {
+
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                if (await DependencyService.Get<IAndroidBluetooth>().Connect(device))
+                {
+                    return true;
+                }
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                // IOS CODE
+            }
+            return false;
+        }
+        public async Task Send(string message)
+        {
+
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                await DependencyService.Get<IAndroidBluetooth>().Send(message);
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                // IOS CODE
+            }
+
+        }
+
+        public async Task<string> Read()
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                return await DependencyService.Get<IAndroidBluetooth>().Read();
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                // IOS CODE
+            }
+
+            return "";
+        }
+        public async Task<bool> SendCredentialsAndGetState(string ssid, string pw, int TimeoutSec)
+        {
+            TimeoutSec *= 1000;
+            Stopwatch stopwatch = new Stopwatch();
+            string message = "";
+            bool connected = false;
+
+            await Task.Run(async () =>
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    await Send("ssid:" + ssid);
+                    await Task.Delay(2000);
+                    await Send("pw:" + pw);
+
+                    stopwatch.Stop();
+                    stopwatch.Reset();
+                    stopwatch.Start();
+
+                    while (message != "connected" && message != "failed" && stopwatch.ElapsedMilliseconds < TimeoutSec / 3)
+                    {
+                        message = await Read();
+                    }
+                    if (message == "connected") { connected = true; break; }
+                }
+            });
+            if (connected) { return true; }
+
+            return false;
+        }
+    }
+
+    static class WiFi
+    {
+        static public string GetIPadress()
+        {
+            string ip = "";
+
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                ip = DependencyService.Get<IAndroidWifi>().GetIp();
+                return ip;
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                // IOS CODE
+            }
+            return ip;
+        }
+
+    }
+    public class Microphone
+    {
+
+        //DependencyService.Get<IAndroidMicrophone>().glob();
+        public async Task<bool> Initialize()
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                return await DependencyService.Get<IAndroidMicrophone>().InitializeMicrophoneAsync();
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                // IOS CODE
+            }
+
+            return false;
+        }
+
+        public async Task Stop()
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                await DependencyService.Get<IAndroidMicrophone>().StopMicrophoneAsync();
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                // IOS CODE
+            }
+        }
+        public async Task Start()
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                await DependencyService.Get<IAndroidMicrophone>().StartMicrophoneAsync();
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                // IOS CODE
+            }
+        }
+
+        public int Read()
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                return DependencyService.Get<IAndroidMicrophone>().GetAmplitude();
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                // IOS CODE
+            }
+            return 0;
+        }
+
+        public bool IsInitialized()
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                return DependencyService.Get<IAndroidMicrophone>().IsInitialized();
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                // IOS CODE
+            }
+            return false;
+        }
+        public bool IsStarted()
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                return DependencyService.Get<IAndroidMicrophone>().IsStarted();
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                // IOS CODE
+            }
+            return false;
+        }
+    }
+
+}
