b131e252ae26937ec5dca5b70ab33a72abcce02d
codrdr <elliotmail21@gmail.com>
2021-11-08 20:44:26 +0100

    Upload animation & other

---
+++ VibeLightApp/VibeLightApp/VibeLightApp/PageAnimation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VibelightApp
+{
+    public static class PageAnimation
+    {
+
+        public static async void Load(StackLayout MainLayout,uint speed)
+        {
+
+            
+            for (int i = 0; i < MainLayout.Children.Count; i++)
+            {
+                await Task.WhenAll
+                (
+                  MainLayout.Children[i].FadeTo(1, speed,Easing.SinInOut),
+                  MainLayout.Children[i].TranslateTo(0, 0, speed, Easing.SinInOut)
+                );
+            }
+
+        }
+       
+        public static async Task Unload(StackLayout MainLayout, uint speed)
+        {
+
+            for (int i = 0; i < MainLayout.Children.Count; i++)
+            {
+                await Task.WhenAll(
+                  MainLayout.Children[i].FadeTo(0, speed, Easing.SinInOut),
+                  MainLayout.Children[i].TranslateTo(0, -20, speed, Easing.SinInOut)
+                  );
+
+            }
+        }
+        public static async Task ButtonPress(Button myBtn, uint speed)
+        {
+            await myBtn.ScaleTo(1.05, speed, Easing.SinInOut);
+
+            await myBtn.ScaleTo(1.0, speed, Easing.SinInOut);
+        }
+
+    }
+}
---
+++ VibeLightApp/VibeLightApp/VibeLightApp/SplashScreen.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VibelightApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SplashScreen : ContentPage
+    {
+        public SplashScreen()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+            BindingContext = this;
+            PageAnimation.Load(MainLayout,100);
+        }
+        protected override void OnAppearing()
+        {
+           
+        }
+        protected override void OnDisappearing()
+        {
+
+        }
+        protected override bool OnBackButtonPressed()
+        {
+
+            base.OnDisappearing();
+            if (Navigation.NavigationStack.Count == 1)
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+            else
+            {
+                Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 1]);
+            }
+
+            GC.Collect();
+            return false;
+        }
+
+
+        private async void Next_Clicked(object sender, EventArgs e)
+        {
+            await Task.WhenAll(
+            PageAnimation.ButtonPress(Next, 100),
+            PageAnimation.Unload(MainLayout, 10)
+
+        );
+
+
+            await Navigation.PushAsync(new ConnectionPage(), false);
+
+            this.Navigation.RemovePage(this.Navigation.NavigationStack[Navigation.NavigationStack.Count - 2]);
+            GC.Collect();
+
+        }
+    }
+}
