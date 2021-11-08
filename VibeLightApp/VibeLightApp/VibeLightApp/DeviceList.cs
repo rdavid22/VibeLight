ee7d7fe155699b11b5f2ace0b9433fe5ca22e0f6
codrdr <elliotmail21@gmail.com>
2021-11-08 20:43:20 +0100

    Upload Devicelist

---
+++ VibeLightApp/VibeLightApp/VibeLightApp/DeviceList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VibelightApp
+{
+    public class DeviceList
+    {
+        public string Device;
+        public string Alias;
+        public string IP;
+        public string Led;
+        public string ListID;
+        public DeviceList(string deviceName, string aliasName, string ipAddress, string ledNumber, string listId)
+        {
+            Device = deviceName;
+            Alias = aliasName;
+            IP = ipAddress;
+            Led = ledNumber;
+            ListID = listId;
+        }
+        public DeviceList()
+        {
+          
+        }
+
+       
+
+    }
+
+}
---
+++ VibeLightApp/VibeLightApp/VibeLightApp/MessageHandler.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+namespace VibelightApp
+{
+    public class MessageHandler : UDP
+    {
+        public async Task<bool> MessageSendAsync(DeviceList device, string message)
+        {
+            if (device == null) { return false; }
+
+            if (message.StartsWith("start:") || message.StartsWith("end") || message.StartsWith("rename:") || message.StartsWith("reset"))
+            {
+                return await ValidationSendAsync(device.IP, message, 3000);
+            }
+
+            else
+            {
+                await SendAsync(device.IP, message);
+                return true;
+            }
+        }
+
+        public async Task<List<DeviceList>> DiscoverDevicesAsync()
+        {
+            List<DeviceList> Devices = new List<DeviceList>();
+
+            await Task.Run(async () =>
+            {
+
+
+                string temp = await BroadcastAsyncAndGetResponse("helo", 4000);
+
+                string[] TempArray = temp.Split('\n');
+
+                for (int i = 0; i < TempArray.Length - 1; i++)
+                {
+                    string[] ParametersArray = TempArray[i].Split(',');
+
+                    DeviceList device = new DeviceList
+                    {
+                        Device = ParametersArray[0],
+                        IP = ParametersArray[1],
+                        Led = ParametersArray[2],
+                        Alias = ParametersArray[3],
+                        ListID = Devices.Count.ToString()
+                    };
+
+                    Devices.Add(device);
+                }
+            });
+
+            return Devices;
+        }
+        public List<DeviceList> DiscoverDevices()
+        {
+            List<DeviceList> Devices = new List<DeviceList>();
+
+
+
+            string temp = BroadcastAndGetResponse("helo", 5000);
+
+            string[] TempArray = temp.Split('\n');
+
+            for (int i = 0; i < TempArray.Length - 1; i++)
+            {
+                string[] ParametersArray = TempArray[i].Split(',');
+
+                DeviceList device = new DeviceList
+                {
+                    Device = ParametersArray[0],
+                    IP = ParametersArray[1],
+                    Led = ParametersArray[2],
+                    Alias = ParametersArray[3],
+                    ListID = Devices.Count.ToString()
+                };
+
+                Devices.Add(device);
+            }
+
+
+            return Devices;
+        }
+    }
+
+    //                                      ඞ
+
+
+}
