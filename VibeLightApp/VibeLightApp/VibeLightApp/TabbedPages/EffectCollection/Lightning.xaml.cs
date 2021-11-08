6d9952e8a683a9e48717868b70a6af0f6f1983ba
poseidon99 <haszonits.gergo@gmail.com>
2021-11-08 20:48:24 +0100

    még 2 effekt

---
+++ VibeLightApp/VibeLightApp/VibeLightApp/TabbedPages/EffectCollection/Lightning.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VibelightApp.TabbedPages.EffectCollection
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Lightning : ContentPage
+    {
+        public bool ZeusIsOn = false;
+        public Lightning()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+            PageAnimation.Load(MainLayout, 30);
+        }
+
+        protected override void OnAppearing()
+        {
+
+        }
+
+        protected async override void OnDisappearing()
+        {
+
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+
+            Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 1]);
+
+            return false;
+        }
+        void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            var data = e.Reading;
+
+            Console.WriteLine(data.Acceleration.X + data.Acceleration.Y + data.Acceleration.Z);
+
+            double Gforce = Math.Abs(data.Acceleration.X + data.Acceleration.Y + data.Acceleration.Z);
+            
+            if (Gforce > 6)
+            {
+                App.Message.MessageSendAsync(App.SelectedDevice,"param: -1, 1");
+            }
+
+        }
+
+       
+
+        private void Zeus_Clicked(object sender, EventArgs e)
+        {
+            if (!ZeusIsOn)
+            {             
+                shake.IsVisible = true;
+                Zeus.BackgroundColor = Color.FromHex("645566");
+                Chance.Value = 0;
+                ToggleOnAccelerometer();
+                ZeusIsOn = true;
+            }
+            else
+            {
+                shake.IsVisible = false;
+                Zeus.BackgroundColor = Color.FromHex("414141");
+                ToggleOffAccelerometer();
+                ZeusIsOn = false;
+            }
+        }
+        private async void OnChanceChanged(object sender, EventArgs e)
+        {
+            await App.Message.MessageSendAsync(App.SelectedDevice, "param: "+ Convert.ToString(Convert.ToInt32(100-Chance.Value)) + ", -1");
+        }
+
+        public void ToggleOnAccelerometer()
+        {
+            try
+            {
+                if (!Accelerometer.IsMonitoring)
+                {
+                    Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+                    Accelerometer.Start(SensorSpeed.Fastest);
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+
+            }  // Feature not supported on device
+           
+        }
+        
+        public void ToggleOffAccelerometer()
+        {
+            try
+            {
+                if (Accelerometer.IsMonitoring)
+                {
+                    Accelerometer.ReadingChanged -= Accelerometer_ReadingChanged;
+                    Accelerometer.Stop();
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+
+            }  // Feature not supported on device
+
+        }
+
+        private void Chance_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ VibeLightApp/VibeLightApp/VibeLightApp/TabbedPages/EffectCollection/Rocket.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VibelightApp.TabbedPages.EffectCollection
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Rocket : ContentPage
+    {
+        public Rocket()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+            PageAnimation.Load(MainLayout, 30);
+        }
+        // !-- lucky rocket: -1,halvanyulas,szin,szineltolasnagysaga,tényerő,kesleltetes-->
+
+        protected override void OnAppearing()
+        {
+            
+        }
+
+        protected async override void OnDisappearing()
+        {
+
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+
+            Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 1]);
+
+            return false;
+        }
+
+        private async void OnFadeChanged(object sender, ValueChangedEventArgs e)
+        {
+            await App.Message.MessageSendAsync(App.SelectedDevice,"param:-1,"+ Convert.ToString(Convert.ToInt32(Fade.Value)) + ",-1,-1,-1,-1");
+        }
+        private async void OnHueChanged(object sender, ValueChangedEventArgs e)
+        {
+        
+            await App.Message.MessageSendAsync(App.SelectedDevice, "param:-1,-1,"+ Convert.ToString(Convert.ToInt32(Hue.Value)) + ",-1,-1,-1");
+            Hue.ThumbColor = Hue.MinimumTrackColor = ColorConverters.FromHsla(Convert.ToSingle(Hue.Value * 1.41), 80, 80, 200);
+
+        }
+        private async void OnDeltaHueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+          await App.Message.MessageSendAsync(App.SelectedDevice, "param:-1,-1,-1," + Convert.ToString(Convert.ToInt32(DeltaHue.Value)) + ",-1,-1");
+
+        }
+        private async void OnBrightnessChanged(object sender, ValueChangedEventArgs e)
+        {
+            await App.Message.MessageSendAsync(App.SelectedDevice, "param:-1,-1,-1,-1," + Convert.ToString(Convert.ToInt32(Brightness.Value)) + ",-1");
+
+        }
+        private async void OnDelayChanged(object sender, ValueChangedEventArgs e)
+        {
+
+            await App.Message.MessageSendAsync(App.SelectedDevice, "param:-1,-1,-1,-1,-1," + Convert.ToString(Convert.ToInt32(Delay.Value)));
+
+        }
+
+
+    }
+}
